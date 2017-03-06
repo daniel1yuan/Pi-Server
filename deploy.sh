57508ce09386d08c5f6b3409ead91a86edcfec99
Daniel Yuan <daniel1yuan@gmail.com>
2017-03-06 02:26:11 +0000

    New production file location

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,12 @@
 #This file is used to deploy this copy of PiServer to production version
 
 #Copy all files from source directory to webapps
-cp -r /home/pi/Documents/Projects/PiServer/PiServer /webapps/PiServer
+cp -r ./PiServer /hdd/webapps/piserver
 
 #Migrate and collect status
-python /webapps/PiServer/PiServer/manage.py migrate
-python /webapps/PiServer/PiServer/manage.py collectstatic
+python /hdd/webapps/piserver/PiServer/manage.py makemigrations
+python /hdd/webapps/piserver/PiServer/manage.py migrate
+python /hdd/webapps/piserver/PiServer/manage.py collectstatic
 
 #Restart Server
 supervisorctl restart PiServer
